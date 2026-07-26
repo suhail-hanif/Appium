0a95d9c55ec8b17d4d112d861460d340eaed8722
AbuMuawiyah <AbuMuawiyah@users.noreply.github.com>
2026-07-26 11:29:44 +0100

    WIP

--- test/specs/test.ts
+++ test/specs/test.ts
@@ -31,14 +31,14 @@ describe('Product detail tests', () => {
     });
 });
 
-// describe('Cart page tests', () => {
-//     it('should navigate to the cart when the cart icon is clicked', async () => {
-//         const cartIcon = await homePage.viewCart;
-//         await cartIcon.waitForDisplayed({ timeout: 10000 });
-//         await cartIcon.click();
+describe('Cart page tests', () => {
+    it('should navigate to the cart when the cart icon is clicked', async () => {
+        const cartIcon = await homePage.viewCart;
+        await cartIcon.waitForDisplayed({ timeout: 10000 });
+        await cartIcon.click();
 
-//         const cartScreenTitle = await cartPage.cartScreenTitle;
-//         await cartScreenTitle.waitForDisplayed({ timeout: 10000 });
-//         await expect(cartScreenTitle).toBeDisplayed();
-//     });
-// });
+        const cartScreenTitle = await cartPage.cartScreenTitle;
+        await cartScreenTitle.waitForDisplayed({ timeout: 10000 });
+        await expect(cartScreenTitle).toBeDisplayed();
+    });
+});
